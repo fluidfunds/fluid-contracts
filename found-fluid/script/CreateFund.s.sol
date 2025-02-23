e23a8d03f03aaebc978c1e427779df0fadbd73e3
Shreyas Papinwar <spapinwar@gmail.com>
2025-02-23 12:52:59 +0530

    feat: deploy graph for fluidfunds, including trade executor

--- found-fluid/script/CreateFund.s.sol
+++ found-fluid/script/CreateFund.s.sol
@@ -3,12 +3,13 @@ pragma solidity ^0.8.19;
 
 import {Script} from "forge-std/Script.sol";
 import {FluidFlowFactory} from "../src/FluidFlowFactory.sol";
+import {ISuperToken} from "@superfluid-finance/ethereum-contracts/contracts/interfaces/superfluid/ISuperToken.sol";
 
 contract CreateFund is Script {
     function run() external {
         address factory = vm.envAddress("FACTORY_ADDRESS");
         address tradeExecutor = vm.envAddress("TRADE_EXECUTOR");
-        address acceptedToken = vm.envAddress("ACCEPTED_TOKEN");
+        ISuperToken acceptedToken = ISuperToken(vm.envAddress("ACCEPTED_TOKEN"));
         
         vm.startBroadcast();
         FluidFlowFactory(factory).createFund(
